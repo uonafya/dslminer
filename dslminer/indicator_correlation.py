5e85f61fccb8e5901893fde90982ece3e2fbedf5
Duncan Ndiithi <duncanndiithi@yahoo.com>
2020-11-20 13:44:05 +0300

    improve multivariate forecast model

--- dslminer/indicator_correlation.py
+++ dslminer/indicator_correlation.py
@@ -1,14 +1,16 @@
 import logging
-import matplotlib.pyplot as plt
 import pandas as pd
-from . import db
+import db
 from datetime import datetime
-from statsmodels.tsa.vector_ar.var_model import VAR
-from statsmodels.tsa.stattools import adfuller
 import datetime
-import math
+from sklearn import linear_model
+from fbprophet import Prophet
+from dateutil.relativedelta import *
+import numpy
 
 # configurations
+from utils import diff_month
+
 log = logging.getLogger("indicator correlation")
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',level=logging.DEBUG)
 
@@ -46,7 +48,7 @@ class IndicatorCorrelation:
         if (len(row) != 0):
             log.info("============================")
             self.end_year=int(row[0][0])
-            if (int(row[0][0] < 2010)):
+            if (int(row[0][1] < 2010)):
                 pass
             else:
                 self.begin_year = int(row[0][1])
@@ -71,6 +73,7 @@ class IndicatorCorrelation:
         indic_payload_values = {}
         added_org_units = []
         added_indics = {}
+        max_indicator_dates = {}
         for row in rows:
 
             if row[2] not in added_indics: # indicator name
@@ -115,6 +118,8 @@ class IndicatorCorrelation:
                 data_element.append(row[0])
                 data_element.append(row[1])
                 indicators_dt[indicatr_id].append(data_element)
+            if(row[0]!=None):
+                max_indicator_dates[indicatr_id]= row[0]
 
         indicators_dataframes = []
         for key in indicators_dt:
@@ -129,7 +134,7 @@ class IndicatorCorrelation:
         final_df = pd.concat(indicators_dataframes, axis=1, sort=False)
         # log.info(final_df.head(50))
         # log.info(indicator_df.head())
-        return (final_df,indic_payload_values,indic_meta_list,org_meta_list)
+        return (final_df,indic_payload_values,indic_meta_list,org_meta_list,max_indicator_dates)
 
 
     def run_correlation(self,indicator_id,orgunit_id,compare_indicators):
@@ -173,120 +178,137 @@ class IndicatorCorrelation:
 
         return result
 
-    '''
-        Makes given series (df_train) stationary to enable for timeseries analysis.
-    '''
-    def make_series_stationary(self, df_train):
-        # ADF Test on each column
-        for name, column in df_train.iteritems():
-            is_stationary = self.adfuller_test(column, name=column.name)
-            # print('\n')
-            if is_stationary == 0 and self.stationarity_iterations_count != 4:
-                df_train = df_train.diff().dropna()
-                self.stationarity_iterations_count = self.stationarity_iterations_count + 1
-                self.make_series_stationary(df_train)
-        return df_train
+
+    def forecast_independent_variable(self,key,indicator_id,time_range,max_indicator_dates,indicator_df):
+            indicator_id = int(indicator_id)
+            time_diff=diff_month(max_indicator_dates[indicator_id] , max_indicator_dates[key]) # diff between last data in independent variable (indicaotor) and the predicted variable(indicator)
+
+            time_diff=time_range+time_diff # to total number of values to be forecasted and filled to the independent variable
+            df_colmn = '%s' %key
+
+            indicator_data_to_fill = indicator_df[df_colmn].truncate(after=max_indicator_dates[key]).to_frame().reset_index() # remove null indcies starting from last date with value to current
+
+            predicted_indicator_data = self.fill_missing_vals(indicator_data_to_fill, time_diff) # fill missing dates with data and also add forecasted with number of values to be predicted in dependent variable
+
+            predicted_values_to_append=predicted_indicator_data.yhat #actual forecasted values
+
+            predicted_values_to_append = predicted_values_to_append.to_frame() #to dataframe
+            predicted_values_to_append.insert(0, "date", pd.date_range(start=max_indicator_dates[key]+relativedelta(months=+1), periods=time_diff,
+                                                        freq='MS'))  # generate new time periods for forecasted data.
+            indicator_data_to_fill.columns = ['date', 'value']
+            indicator_data_to_fill.set_index('date')
+            predicted_values_to_append.columns = ['date', 'value']
+            predicted_values_to_append.set_index('date')
+            concatnated_df = pd.concat([indicator_data_to_fill, predicted_values_to_append])
+            concatnated_df.set_index('date',drop=True,inplace=True)
+            concatnated_df.index = pd.DatetimeIndex(concatnated_df.index) #make index datetime
+            return concatnated_df
+
 
     '''
        implementation of Vector autoregression model for multivariate prediction analysis.
     '''
     def run_var_prediction(self,indicator_id,orgunit_id,compare_indicators,time_range):
 
-        compare_indicators = str(indicator_id) + ',' + compare_indicators
+        parameter_indicators = str(indicator_id) + ',' + compare_indicators
         self.set_max_min_period(orgunit_id, indicator_id)
-        indic_data = self.get_indicator_data(orgunit_id, compare_indicators)
+        indic_data = self.get_indicator_data(orgunit_id, parameter_indicators)
         indicator_df = indic_data[0]
-        indicator_df = indicator_df.fillna(indicator_df.mean()) # fill NaN with averages.
+        max_indicator_dates = indic_data[4]
 
-        #we split the data frame to training and test series
-        nobs = 4
-        df_train, df_test = indicator_df[0:-nobs], indicator_df[-nobs:]
+        indipendent_variable =[]
+        for indic_id in max_indicator_dates:
+            if (indic_id != indicator_id):
+                forecasted_indepn_var=self.forecast_independent_variable(indic_id, indicator_id, time_range, max_indicator_dates, indicator_df)
+                indipendent_variable.append(forecasted_indepn_var)
 
-        #make the series stationary
-        df_differenced= self.make_series_stationary(df_train)
-        model = VAR(df_differenced)
-        # fit model
-        model_fitted = model.fit()
+        indicator_df.fillna(indicator_df.mean(),inplace=True)  # fill NaN with averages.
 
-        # Get the lag order
-        lag_order = model_fitted.k_ar
+        indicator_colmn = '%s' % indicator_id
+        indicator_df[indicator_colmn].index = pd.DatetimeIndex(indicator_df[indicator_colmn].index)
 
-        # Input data for forecasting
-        forecast_input = df_differenced.values[-lag_order:]
+        independent_variables_vals = []
+        independent_predicted_vals = []  # we  create a 2d array to hold values for the X independent to be used for against correlation model to do predcition of Y variable
+        independ_correlation_count = len(indipendent_variable[0])-time_range # control to put values for fitting and for X correlation prediction separate
+        correlation_arr_counter = 0
 
-        # Forecast
-        forecast_data = model_fitted.forecast(y=forecast_input, steps=time_range)
+        for x in range(len(indipendent_variable[0])):
+            if independ_correlation_count!=0:
+                for data_frame_no in range(len(indipendent_variable)):
+                    if len(independent_variables_vals) != len(indipendent_variable): # create list for each indicators data
+                        independent_variables_vals.append([])
 
-        df_forecast = pd.DataFrame(forecast_data, columns=indicator_df.columns)
-        predicted_results = self.invert_transformation(df_train, df_forecast)
+                    independent_variables_vals[data_frame_no].append(indipendent_variable[data_frame_no].value[x])
+                independ_correlation_count = independ_correlation_count - 1
+            else:
+                independent_predicted_vals.append([])
+                for data_frame_no in range(len(indipendent_variable)):
+                    independent_predicted_vals[correlation_arr_counter].append(indipendent_variable[data_frame_no].value[x])
+                correlation_arr_counter = correlation_arr_counter + 1
 
-        return (predicted_results,indic_data)
+        strt_date = str(self.begin_year) + "-01-01"
+        indep_dataframes = []
+
+        for dat_frame in independent_variables_vals:
+            arr = numpy.array(dat_frame)
+            indep_dataframe=pd.DataFrame(arr)
+            indicator_dframe = indicator_df[indicator_colmn]
+            indicator_dframe=indicator_dframe.truncate(after=max_indicator_dates[indicator_id]) # remove null values if any in indicator (dependent var) to  be predicted
+            #indicator_dframe.round(decimals=3)
+            indep_dataframe.insert(0, "date", pd.date_range(start=strt_date, periods=len(indicator_dframe), freq='MS'))  # generate new time periods for forecasted data.
+            indep_dataframe = indep_dataframe.set_index('date')
+            indep_dataframe.fillna(indep_dataframe.mean(),inplace=True)
+            indep_dataframes.append(indep_dataframe)
+
+        indep_dataframes = pd.concat(indep_dataframes, axis=1, sort=False)
+        X=indep_dataframes
+        Y = indicator_dframe # value to be forecasted - multivariate
+        regr = linear_model.LinearRegression()
+        X=X.values
+        Y=numpy.array(Y.values)
+        regr.fit(X,Y)
+        forecast_data=regr.predict(independent_predicted_vals)
+        #, columns=indicator_df.columns
+        df_forecast = pd.DataFrame(forecast_data)
+
+        return (df_forecast,indic_data)
 
 
-    def invert_transformation(self,df_train, df_forecast):
-        """Revert back the differencing to get the forecast to original scale."""
-        df_fc = df_forecast.copy()
-        columns = df_train.columns
-        for col in columns:
-            # Roll back Diff value
-            if self.stationarity_iterations_count==1:
-                # Roll back 1st Diff
-                df_fc[str(col)] = df_train[col].iloc[-1] + df_fc[str(col)].cumsum()
-            elif  self.stationarity_iterations_count>1:
-                for i in range(self.stationarity_iterations_count,1,-1):
-                    df_fc[str(col)] = (df_train[col].iloc[-i] - df_train[col].iloc[-(i+1)]) + df_fc[str(col)].cumsum()
-                    if i==1:
-                        df_fc[str(col)] = df_train[col].iloc[-1] + df_fc[str(col)].cumsum()
+    '''
+        projects missing values from given series data
+    '''
+    def fill_missing_vals(self,series, leap):
+        m = Prophet()
+        series.columns = ['ds','y']
+        m.fit(series)
+        future = m.make_future_dataframe(periods=leap, freq='M',include_history = False)
+        forecast = m.predict(future)
+        return forecast
 
-
-        return df_fc
-
-    def adfuller_test(self,series, signif=0.05, name='', verbose=False):
-        """Perform ADFuller to test for Stationarity of given series and print report"""
-        r = adfuller(series, autolag='AIC')
-        output = {'test_statistic': round(r[0], 4), 'pvalue': round(r[1], 4), 'n_lags': round(r[2], 4), 'n_obs': r[3]}
-        p_value = output['pvalue']
-
-        def adjust(val, length=6):
-            return str(val).ljust(length)
-
-        # Print Summary
-        print(f'    Augmented Dickey-Fuller Test on "{name}"', "\n   ", '-' * 47)
-        print(f' Null Hypothesis: Data has unit root. Non-Stationary.')
-        print(f' Significance Level    = {signif}')
-        print(f' Test Statistic        = {output["test_statistic"]}')
-        print(f' No. Lags Chosen       = {output["n_lags"]}')
-
-        for key, val in r[4].items():
-            print(f' Critical value {adjust(key)} = {round(val, 3)}')
-
-        if p_value <= signif:
-            print(f" => P-Value = {p_value}. Rejecting Null Hypothesis.")
-            print(f" => Series is Stationary.")
-            return 0
-        else:
-            print(f" => P-Value = {p_value}. Weak evidence to reject the Null Hypothesis.")
-            print(f" => Series is Non-Stationary.")
-            return 1
 
     def do_multivariate_prediction(self,indicator_id,orgunit_id,compare_indicators,time_range):
         time_range = int(time_range)
         # indic_data = final_df,indic_payload_values,indic_meta_list,org_meta_list
         # var_data = predicted_results,indic_data
+        indicator_id =int(indicator_id)
         var_data = self.run_var_prediction(indicator_id,orgunit_id,compare_indicators,time_range)
         # end_forecast_date =var_data[1][0].index[-1] + datetime.timedelta(days=time_range)
         start_forecast_date = var_data[1][0].index[-1] + datetime.timedelta(days=1)
+
         var_data[0].insert(0, "date", pd.date_range(start = start_forecast_date, periods=time_range, freq= 'MS')) # generate new time periods for forecast data.
+
         predicted_dataframe = var_data[0].set_index("date")
-        final_data = pd.concat([var_data[1][0], predicted_dataframe])
+        # final_data = pd.concat([var_data[1][0], predicted_dataframe])
+        final_data = var_data[1][0]
         period_span = {"start_date": str(self.begin_year), "end_date": str(self.end_year)}
-        print("================")
         indicator_column = '%s' %indicator_id
         forecast_payload_values = {}
         forecast_payload_values[indicator_column] = []
-        for items in predicted_dataframe[indicator_column].iteritems():
+
+        for index, row in predicted_dataframe.iterrows():
             indict_list = forecast_payload_values[indicator_column]
-            indic_val = {"date": str(items[0].date()), "value": str(round(items[1],2)), "ouid": str(orgunit_id)}
+            date_time = index.strftime("%Y-%m-%d")
+            indic_val = {"date": date_time, "value": str(round(row[0], 2)), "ouid": str(orgunit_id)}
             indict_list.append(indic_val)
 
         # assemble result
@@ -309,5 +331,5 @@ class IndicatorCorrelation:
 
 
 # r=IndicatorCorrelation()
-# r.do_multivariate_prediction(23185,23408,'23191,23701,31589',10)
-# r.run_var_prediction(93299,23408,'32954,2449433',10) #,2449433,93323
+# r.do_multivariate_prediction(32956,18,'32413',30) # 21030, '23191,23701,31589'
+# r.run_var_prediction(21030,23408,'93299,23700,23191,31584',30) #,2449433,|93299,
