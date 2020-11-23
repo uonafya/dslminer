3e708664798256a2eaf6616c0801eed4d818601c
Duncan Ndiithi <duncanndiithi@yahoo.com>
2020-11-23 19:46:14 +0300

    date diff utility

--- dslminer/utils.py
+++ dslminer/utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def make_date(datestring):
     year = datestring[0:4]
     month = datestring[4:6]
@@ -7,3 +9,9 @@ def make_date(datestring):
 
 def diff_month(d1, d2):
     return (d1.year - d2.year) * 12 + d1.month - d2.month
+
+def months_between(d1, d2):
+    # d1 = datetime.strptime(d1, "%Y-%m-%d")
+    # d2 = datetime.strptime(d2, "%Y-%m-%d")
+    num_months = (d2.year - d1.year) * 12 + (d2.month - d1.month)
+    return abs(num_months)
